98785617ab6f8e9609306ae355b3df60aa3ca31f
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-02-22 17:02:51 -0300

    primeiras aulas de js

---
+++ Js/Seção 2 js básico/aula 3/index.js
@@ -0,0 +1,13 @@
+/* console.log('Francisco Junior'); string 
+console.log("Francisco Junior");
+console.log(`Francisco Junior`);
+
+console.log(35 , 15.85, 'Francisco Junior'); /*number */
+  console.log('Meu nome é "Francisco Junior". Estou aprendendo JavaScript ás',20,'da noite');
+
+
+
+
+
+
+  
