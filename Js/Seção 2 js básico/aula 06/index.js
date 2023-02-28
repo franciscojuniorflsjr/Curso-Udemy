8b237b170515b215ed31f52db6c5af047a3a0b6f
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-02-28 09:22:29 -0300

    variáveis

---
+++ Js/Seção 2 js básico/aula 06/index.js
@@ -0,0 +1,35 @@
+ let nome = 'Júnior';
+
+
+console.log (nome, 'nasceu em 1989.')
+console.log ('Em 2015 conheceu Daiane ');
+console.log (nome,'casou-se com Daiane em 2020.')
+console.log ('Daiane teve 1 filho com',nome,' em 2022.')
+console.log('O filho de',nome,' se chama Eduardo.')
+
+
+// Não podemos criar variáveis com palavras reservadas
+//Variáveis precisam ter nomes significativos
+// Não pode comecar o nome de variável com um número
+//Não podem conter espaços ou traços
+
+/* Ultilizamos camelCase 'primeira palavra toda em minuscula em seguidaprimeira letra maiuscula resto minuscula'
+let nomeCompletoDoCliente = 'Júnior lopes';    
+console.log(nomeCompletoDoCliente);
+*/
+
+// Case-sensitive 'letra maiúscula e minúscula faz diferença'
+/*
+let nomeCompleto = 'Francisco'
+let nomecompleto ='Júnior'
+console.log(nomeCompleto,nomecompleto);
+*/
+
+/* Não podemos redeclarar variáveis com let 
+
+let nomeCliente = 'Júnior';
+nomeCliente = 'Francisco';
+console.log(nomeCliente);
+
+*/ 
+// NÃO UTILIZE VAR, UTILIZE LET.
