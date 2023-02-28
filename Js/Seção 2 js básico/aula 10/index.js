49042245b16807a05612cb6a7d7b992a442295b5
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-02-28 12:01:43 -0300

    aula 10 Tipos de dados Primitivos

---
+++ Js/Seção 2 js básico/aula 10/index.js
@@ -0,0 +1,18 @@
+// Tipo de dados Primitivos
+// string , number , undefined ,null, boolean
+
+const nome = 'Francisco' ;//string
+const nome1 = 'Francisco' ;//string
+const nome2 =  `Francisco` ;//string
+const num1 = 10; //number
+const num2 = 10.52; // number
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null // Nulo -> não aponta para local nenhum na memória
+const aprovado = false; // Boolean = true = "verdadeiro" false = 'falso' (valor lógico)
+
+//consulta o typeof
+//console.log(typeof nome , nome);
+
+const a = [1, 2 ]
+const b = a ;
+console.log (a,b)
