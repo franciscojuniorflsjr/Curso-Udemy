e1eea4bee76d72fa4ae635588402c52f9b5fcc02
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-02-28 18:44:51 -0300

    Operadores aritméticos  é operadores de atribuição

---
+++ Js/Seção 2 js básico/aula 11/index.js
@@ -0,0 +1,37 @@
+/**
+ * Operadores Aritiméticos
+ *  + Adição / concatenação
+ *  -Subtração  /divide  *Multiplicação  **Potênciação
+ *  % Resto da divisão
+ * 
+ *  ORDEM DE PRECEDÊNCIA ARITMÉTICOS
+  ---------------------------------
+   ()
+   **
+   * / %
+   + -
+ ---------------------------
+   Incremento = ++
+   Decremento = --
+
+   let contador = 1;
+    contador++ 
+    console.log(contador) 
+-----------------------------
+ Operadores de atribuição
+
+let contador = 2
+contador += 2
+contador += 2 
+contador += 2 
+console.log (contador)
+---------------------------------------------------
+// NaN - Not a Number parseInt(inteiro) parseFloat(decimais) Number(escolha automatica numeros inteiros e decimais)
+*/
+const num1 = 10
+const num2 = Number('5.6')
+console.log(num1 + num2) 
+console.log(typeof num2)
+
+ 
+
