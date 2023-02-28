2d27b4ceabbd06a9f6341714a821fdb5100ba2f4
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-02-28 11:31:46 -0300

    Diferenças entre Var e Let

---
+++ Js/Seção 2 js básico/aula 09/index.js
@@ -0,0 +1,16 @@
+
+// Diferenças entre Var e Let
+
+// var pode ser substituido o nome da variável
+var nome = 'Francisco'
+nome = 'Júnior'
+console.log(nome)
+
+//NÃO PODE TER LET COM O MESMO NOME
+// CASO QUEIRA ALTERAR NOME DA VARIÁVEL
+
+let nome = 'Francisco'
+// let nome = 'Júnior' ( não pode let com o mesmo nome)
+nome = 'Junior' // ta ok pode alterar o nome da variável
+
+console.log(nome)
