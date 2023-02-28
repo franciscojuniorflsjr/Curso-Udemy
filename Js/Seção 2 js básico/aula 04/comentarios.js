859a21eda60d5765f68c24c25f1c2df01a38faec
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-02-28 08:25:22 -0300

    atualização ex js

---
+++ Js/Seção 2 js básico/aula 04/comentarios.js
@@ -0,0 +1,15 @@
+// E escrever um comentário
+console.log('Olá, mundo!')
+console.log('linha 1') // Aqui tem outro comentário
+console.log('linha 2')
+console.log('linha 3')
+console.log('linha 4')
+// vai ser ignorado
+
+/*
+console.log('Olá, mundo!')
+console.log('linha 1') // Aqui tem outro comentário
+console.log('linha 2')
+console.log('linha 3')
+console.log('linha 4')
+*/
