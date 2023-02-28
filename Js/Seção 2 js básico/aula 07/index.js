11d7eca448bb3042920527cd1db4dcf9a1bf160b
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-02-28 10:03:53 -0300

    aula sobre  Constante

---
+++ Js/Seção 2 js básico/aula 07/index.js
@@ -0,0 +1,23 @@
+
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode comecar o nome de um constante com um número
+// Não podem conter espaços ou traços
+// Ultilizamos camelCase 
+// Case-sensitive 'letra maiúscula e minúscula faz diferença'
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+// Alguns peradores aritméticos   + - * / 
+// String = Text | Number = Número
+ 
+const primeiroNumero = 5; //  5 Number | '5' String
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2 ;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(typeof primeiroNumero);
+
+
