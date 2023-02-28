cc2aa593d2c95ce7442a25a564ce29fdf24c9459
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-02-28 11:08:37 -0300

    Template  String

---
+++ Js/Seção 2 js básico/aula 08/index.js
@@ -0,0 +1,22 @@
+const nome = 'Francisco'
+const sobrenome = 'lopes'
+const idade = '33'
+const peso = '84'
+const alturaEmM = '1.80'
+
+let indeceMassaCorporal; // peso / altura*altura
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM)
+//console.log (imc)
+
+let anoNacimento = 2022 -idade;
+//console.log (anoNacimento)
+
+// TEMPLATE STRING
+
+console.log(`${nome}  ${sobrenome} tem ${idade} anos pesa, ${peso}kg`)
+console.log (`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`)
+console.log(`${nome} nasceu em ${anoNacimento}.`)
+
+
+
+
