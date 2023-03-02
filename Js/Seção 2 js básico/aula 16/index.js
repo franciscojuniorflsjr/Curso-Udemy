5e20b8d8abcbd834363998d7dc192643e5bf9139
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-02 15:18:24 -0300

    aula 15   Array

---
+++ Js/Seção 2 js básico/aula 16/index.js
@@ -0,0 +1,40 @@
+// Array
+//                0        1        2         3        4
+const alunos = ['Luiz', 'Maria', 'João']; // luiza,  Mariana
+
+alunos.push('Luiza')
+alunos.push('Marina')
+
+
+// console.log(alunos.slice(0,3)) // escolhe o primeiro 3 alunos
+// console.log(alunos.slice(0,-1,)) // escolhe os ultimos 2 alunos
+
+
+// console.log(alunos[50]);// undefined
+
+// delete alunos[1] // delete alunos com o indice 1
+// console.log(alunos)
+
+// const removido = alunos.pop() // remove o ultimo elemento do array
+//const removido = alunos.shift()//remove o primeiro elemento do array
+// console.log(removido)
+// console.log(alunos)
+
+// alunos.unshift('Pedro'); // adiciona no inicio
+// alunos.unshift('Francisco');// adiciona no inicio
+
+// alunos.push('Pedro') // adiciona ao final do array
+// alunos.push('Maria') // adiciona ao final do array
+
+// alunos[alunos.length] = 'Paulo'  // adiciona ao final do array
+// alunos[alunos.length] = 'Pedro'// adiciona ao final do array
+// alunos[alunos.length] = 'Junior'// adiciona ao final do array
+
+//console.log(alunos.length) // verifica o tamanho do array
+
+// alunos[0] = 'Francisco'  // altera o primeiro elemento do array
+// alunos[3] = 'Daiane' // altera o último elemento do array
+
+// console.log(alunos)
+// console.log(alunos[0])
+// console.log(alunos[2])
