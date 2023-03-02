b7e27976e1bc1ccef0bf07c4b8d1a7bbd7a604f1
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-02 09:29:57 -0300

    aula 15  Objeto  Math

---
+++ Js/Seção 2 js básico/aula 15/index.js
@@ -0,0 +1,19 @@
+let num1 = 9.54578
+
+
+// let num2 = Math.floor(num1)  arredonda para baixo
+// let num2 = Math.ceil(num1)   arredonda para cima
+// let num2 = Math.round(num1)  arredonda para o mais próximo
+
+// console.log(Math.max(1,2,3,4,5,6,50,110,230,1500,706,90-250,-1))
+// console.log(Math.min(1,2,3,4,5,6,50,110,230,1500,706,90,250,-1))
+
+/*Numeros aleatorios arredondados
+const aleatorio = Math.round(Math.random() * (10 -5) + 5)
+ console.log(aleatorio) 
+*/
+
+// Numero aleatorios
+ const aleatorio = Math.random() * (10 -5) + 5   //de 10 a 5 
+ console.log(aleatorio) 
+ 
