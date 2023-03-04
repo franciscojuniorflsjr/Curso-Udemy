43dbfd162a0f71a4f1bcfb9f711715ff730de2ed
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-04 12:13:51 -0300

    aula de function

---
+++ Js/Seção 2 js básico/aula 17/index.js
@@ -0,0 +1,51 @@
+
+
+
+/*
+const raiz = function (n){
+    return n ** 0.5
+}
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+*/
+
+/*
+const raiz = n => n ** 0.5
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+/*
+
+/*
+function soma (x = 1, y = 1) {
+    const resultado = x + y 
+    return resultado
+}
+    const resultado = soma(4,2)
+    console.log(resultado)
+*/
+
+
+function soma (x, y) {
+const resultado = x + y 
+return resultado // Nada será executado apos a palavra return
+}
+
+const resultado = soma(2, 2)
+console.log(resultado)
+
+
+// console.log(soma(2,2))
+// console.log(soma(3,1))
+// console.log(soma(5,10))
+
+/*
+function saudacao (nome) {
+     return `Bom dia ${nome}!`
+}
+const variavel = saudacao('Junior') 
+console.log(variavel)
+*/
