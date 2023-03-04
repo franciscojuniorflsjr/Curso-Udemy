47e7dbe03dc20064e4ac9d2437a0b0597351df83
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-04 18:06:31 -0300

    ex 20

---
+++ Js/Seção 2 js básico/aula 20/js/script.js
@@ -0,0 +1,39 @@
+function meuEscopo() {
+const form = document.querySelector('.form');
+const resultado = document.querySelector('.resultado');
+
+const pessoas = []
+
+// form.onsubmit = function(evento) {
+// evento.preventDefault();
+// alert(1)
+// console.log('Foi enviado.')
+// }
+
+function recebeEvetoForm (evento) {
+evento.preventDefault();
+const nome = form.querySelector('.nome');
+const sobrenome = form.querySelector('.sobrenome');
+const peso = form.querySelector('.peso');
+const altura = form.querySelector('.altura');
+
+pessoas.push ({
+nome: nome.value,
+sobrenome: sobrenome.value,
+peso: peso.value,
+altura: altura.value
+})
+
+console.log(pessoas)
+
+resultado.innerHTML += ` <P> ${nome.value} ${sobrenome.value}  `
+    `${peso.value} ${altura.value} </P>`
+
+
+
+console.log(nome, sobrenome, peso, altura )
+}
+
+form.addEventListener('submit', recebeEvetoForm)
+}
+meuEscopo();
