6a55977ddd8af9cd17db128eaafe06689a621c28
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-06 10:52:41 -0300

    Operadores Lógicos

---
+++ Js/Seção 3  js lógica de programação/aula 22/index.js
@@ -0,0 +1,21 @@
+/*
+Operadores Lógicos
+---------------------------------------------------
+
+&& -> AND -> E    -> Todas as expressões precisam ser verdadeiras para retornar true "verdadeira" 
+
+const expressaoAnd = true && true && true && true
+console.log(expressaoAnd)
+-----------------------------------------------------
+
+|| -> OR -> OU   -> Basta uma expressão ser true para retornar true caso seja todas false retorna false
+
+const expressaoOr = true || true || true || true
+console.log(expressaoOr)
+-----------------------------------------------------
+
+! -> NOT -> NÃO  -> (!true se torna false)  (!false se torna true)
+ (!! true se torna false e volta true) 
+
+
+*/
