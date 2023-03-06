da75f4d28b61e3fb9ee8851c883c55fc51bb4647
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-06 10:24:40 -0300

    Operadores de comparação

---
+++ Js/Seção 3  js lógica de programação/aula 21/index.js
@@ -0,0 +1,12 @@
+/* 
+Operarores de comparação
+> maior que
+=> maior ou igual a 
+< menor que
+<= menor ou igual a 
+== igualdade (valor) não e recomendado o uso  ****
+=== iguadade estrita  (valor e tipo)
+!= diferente (valor) não e recomendado o uso ***
+!== diferente estrita (valor e tipo)
+
+*/
