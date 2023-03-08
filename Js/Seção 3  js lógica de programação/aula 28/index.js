d39af8785636ba880bdf252ed7e5f32ea724e66b
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-08 11:37:25 -0300

    atualização

--- Js/Seção 3  js lógica de programação/aula 28/index.js
+++ Js/Seção 3  js lógica de programação/aula 28/index.js
@@ -5,14 +5,44 @@
 //const data = new Date (0 + tresHoras + umDia)// 01/01/1970 //Timestamp unix ou época unix
 //const data = new Date(2023, 2, 8, 10,)// a, m, d, h, M, s, ms
  //const data = new Date() // data atual 
-const data = new Date('2023-03-08 10:57:02')
-console.log ('Dia', data.getDate())
-console.log ('Mês', data.getMonth())
-console.log ('Ano', data.getFullYear())
-console.log ('Hora', data.getHours())
-console.log ('Minutos', data.Minutes())
-console.log ('Segundos', data.getSeconds())
-console.log ('MS', data.getMilliseconds())
-console.log ('Dia Semana', data.getDay())
+function zeroAEsquerda(num){
+return num >= 10 ? num : `0${num}`
+}
+function formataData(data){
+const dia = zeroAEsquerda(data.getDate())
+const mes = zeroAEsquerda(data.getMonth() +1)
+const ano = zeroAEsquerda(data.getFullYear())
+const hora = zeroAEsquerda(data.getHours())
+const min = zeroAEsquerda(data.getMinutes())
+const sec = zeroAEsquerda(data.getSeconds())
 
-console.log(data.toString())
+return `${dia}/${mes}/${ano} ${hora}:${min}:${sec}`
+}
+
+const data = new Date()
+const dataBrasil = formataData (data)
+console.log(dataBrasil)
+
+
+
+
+
+
+
+
+
+
+/*
+const data = new Date('2023-03-08 10:57.02');
+console.log ('Dia', data.getDate());
+console.log ('Mês', data.getMonth()) // Mês começa do 0
+console.log ('Ano', data.getFullYear());
+console.log ('Hora', data.getHours());
+console.log ('Min', data.Minutes());
+console.log ('Seg', data.getSeconds());
+console.log ('MS', data.getMilliseconds());
+console.log ('Dia Semana', data.getDay()); // 0 = Domingo, 
+
+console.log(data.toString());
+console.log(Date.now())
+*/
