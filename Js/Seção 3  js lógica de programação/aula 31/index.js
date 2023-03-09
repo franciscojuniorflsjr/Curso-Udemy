316c01d432ff15bc5143be505e8dd398b73425f0
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-09 14:25:02 -0300

    Diferenças entre var e let

---
+++ Js/Seção 3  js lógica de programação/aula 31/index.js
@@ -0,0 +1,23 @@
+//const verdadeiro = true;
+
+// Let tem escopo de bloco {....bloco  }
+// Var só tem escopo de função
+/*
+
+let nome = 'Francisco'; // Criando
+var nome2 = 'Junior'    //Criando
+
+
+if (verdadeiro) {
+    let nome = 'Lopes' // Criado
+    var nome2 = 'Paulo' //Redeclarado  
+    console.log(nome, nome2)
+
+    if (verdadeiro) {
+        var nome2 = 'Francisco' //Redeclarado  
+        let nome = 'Outra coisa'
+        
+    }
+}
+console.log(nome, nome2)
+*/
