91ba0a6f504fe3af79ed43bea29a1244359cc535
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-10 17:55:21 -0300

    aula 34  For  - Clássico - Estrutura de Repetição

---
+++ Js/Seção 3  js lógica de programação/aula 34/index.js
@@ -0,0 +1,31 @@
+// For - Classico - Estrutura de Repetição
+/*
+console.log('Linha 0');
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+*/
+/*
+//  i - inde  - condição  - incremento ++ somar
+for (let i = 0; i <=5; i++) {
+  console.log(`Linha ${i}`)
+}
+*/
+/*
+for (let i = 400; i <=500; i += 10) {
+  console.log(`Linha ${i}`)
+}
+*/
+/*
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar'
+    console.log(i, par)
+}
+*/
+//                0       1       2  
+const frutas = ['Maça', 'Pera', 'Uva','Banana','Pesego','Morango']
+for (let i = 0; i < frutas.length; i++) {
+console.log(`Índice ${i}`, frutas[i])
+}
