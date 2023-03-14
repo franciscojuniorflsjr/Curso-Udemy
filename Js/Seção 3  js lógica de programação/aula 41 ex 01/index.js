6e6f975314980d86920d8cdc93279b11eb251836
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-14 09:10:15 -0300

    aula 41 exercício 1

---
+++ Js/Seção 3  js lógica de programação/aula 41 ex 01/index.js
@@ -0,0 +1,41 @@
+// Escreva uma função que recebe 2 números e retorne  maior deles.
+
+// opção 1
+const max2 = (x, y) => x > y ? x : y;
+/*
+// opção 2
+const max2 = (x, y) => {
+    return x > y ? x : y;
+}
+*/
+console.log(max2(10, 20))
+
+
+
+// opção 1
+/*
+function max(x, y){ return x > y ? x : y;
+    }
+    console.log(max(10, 2))
+*/
+//opção 2
+/*
+function max(x, y){
+    if (x > y) return x; return y;
+    }
+*/
+
+//opção 3
+/*
+function max(x, y){
+if (x > y){
+    return x;
+}else {
+    return y;
+}
+}
+*/
+//console.log(max(10, 2))
+
+//const maior = max(1,2)
+//console.log(maior)
