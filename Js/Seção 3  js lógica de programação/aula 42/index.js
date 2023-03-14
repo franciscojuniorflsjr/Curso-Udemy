753942129bcb25564b48ff3d711fef50dce58551
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-14 09:10:29 -0300

    aula 42 exercício 2

---
+++ Js/Seção 3  js lógica de programação/aula 42/index.js
@@ -0,0 +1,18 @@
+// Escreva uma função chamada ePaisagem que
+// recebe dois argumentos, largura e altura.
+// de uma immagem (number)
+// Retorne true se a imagem estier no paisagem
+
+
+// arrow function opção 1
+
+const ePaisagem = (largura, altura) => largura > altura;
+console.log > altura
+
+// opção 2
+/*
+function ePaisagem(largura, altura) {
+    return largura > altura  // ? true : false;
+}
+console.log(ePaisagem(1980, 1080));
+*/
