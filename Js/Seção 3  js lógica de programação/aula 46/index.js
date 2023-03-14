4cf56c1dccdf2364ba2dd708b6ea99dbdf58adb0
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-14 11:34:49 -0300

    aula 46 setInterval e setTimeout

---
+++ Js/Seção 3  js lógica de programação/aula 46/index.js
@@ -0,0 +1,27 @@
+// setInterval e setTimeout
+
+function mostraHora() {
+   let data = new Date();
+
+   return data.toLocaleTimeString('pt-BR', {
+    hour12: false
+   })
+}
+
+const timer = setInterval(function() {
+    console.log(mostraHora());
+}, 1000)
+
+setTimeout(function()  {
+    clearInterval(timer)
+}, 10000)  // parar ápos 10 segundos
+
+setTimeout(function() {
+ console.log('Olá mundo!');
+}, 5000); // adicionar ápos 5 segundos msg
+/*
+function funcaoDoIntervalo(){
+console.log(mostraHora());
+}
+setInterval(funcaoDoIntervalo, 1000);
+*/
