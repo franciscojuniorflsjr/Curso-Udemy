c6b0ff77fa749384b674c5fa3e003add7f1df02e
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-03-20 17:20:45 -0300

    atualização do exercício 01 aula 47

--- Js/Seção 3  js lógica de programação/aula 47/assets/js/main.js
+++ Js/Seção 3  js lógica de programação/aula 47/assets/js/main.js
@@ -1,15 +1,14 @@
 // new Date(0)
+function relogio() {
 function criaHoraDosSegundos(segundos) {
 const data = new Date(segundos * 1000);
-return data.toLocaleTimeString('pt-BR',{
+return data.toLocaleTimeString('pt-BR', {
     hour12: false, 
-    timeZone: 'UTC'
-})
+    timeZone: 'GMT' // UTC
+});
 }
+
 const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
 let segundos = 0;
 let timer;
 
@@ -21,28 +20,26 @@ timer = setInterval (function() {
 }
 
 document.addEventListener('click' , function(e) { //(e) = evento
-const elemento = e.target;    
+const el = e.target;    //el = elemento
 
-if (elemento.classList.contains('zerar')) {
-    console.log('você clicou em zerar');
-})
+if (el.classList.contains('zerar')){
+    relogio.classList.remove('pausado')
+    clearInterval(timer);
+    relogio.innerHTML ='00:00:00';
+    segundos = 0;
+}
 
-}   
+if (el.classList.contains('pausar')){
+    relogio.classList.add('pausado')    
+    clearInterval(timer);
 
-iniciar.addEventListener('click', function(event) {
-relogio.classList.remove('pausado')    
-clearInterval(timer);
-iniciaRelogio();
-})
+}
 
-pausar.addEventListener('click', function(event) {
-clearInterval(timer);
-relogio.classList.add('pausado')  
-})
-
-zerar.addEventListener('click', function(event) {
-relogio.classList.add('pausado')   
-clearInterval(timer);
-relogio.innerHTML ='00:00:00';
-segundos = 0;
-})
+if (el.classList.contains('iniciar')){
+    relogio.classList.remove('pausado')   
+    clearInterval(timer);
+    iniciaRelogio();
+}
+}); 
+}
+relogio();
